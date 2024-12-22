e0df2271935c7f0daa16e0117f52bb3896915bbc
Marcus Wright <marcus03wright@icloud.com>
2024-12-21 20:08:56 -0500

    Updated usage

--- example_usage.py
+++ example_usage.py
@@ -7,7 +7,9 @@ def main():
     image = Image.open("./test_images/image.jpg")
     mask = Image.open("./test_images/mask.png")
 
-    lama.inpaint(image, mask)
+    inpainted_image = lama.inpaint(image, mask)
+
+    inpainted_image.save("inpainted.jpg")
 
 if __name__ == '__main__':
     main()
